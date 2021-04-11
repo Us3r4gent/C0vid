cd4250d35ef490ae97449d2dbac24662b8e18a55
Michael Fang <66933987+FangziOMG@users.noreply.github.com>
2021-04-11 12:17:50 +0800

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:c0vid/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(Mypp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
